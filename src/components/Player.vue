a1621b8e36aee9c76094e3d0ff5c5e013df0191b
Gabriel F. Xavier <gfzxavier@outlook.com>
2023-02-13 17:22:55 -0300

    feat: WIP create a character and player system

---
+++ src/components/Player.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="player">
+    <knight :action="command"></knight>
+    <!-- <knight :action="'stand'"></knight>
+    <knight :action="'running'"></knight>
+    <knight :action="'attacking'"></knight> -->
+  </div>
+</template>
+
+<script>
+import knight from './characters/Knight.vue'
+export default {
+  name: 'player1',
+  components:{
+    knight
+  },
+  data(){
+    return {
+    }
+  },
+  computed: {
+    command() {
+      if(this.$store.getters.getPressedButton == 'arrowright') {
+        return 'running'
+      }
+      if(this.$store.getters.getPressedButton == 'z') {
+        return 'attacking'
+      }
+      return 'stand'
+    },
+  }
+}
+</script>
+
+<style lang="scss">
+  .player {
+    position: absolute;
+    display: flex;
+  }
+</style>
---
+++ src/components/characters/Knight.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="knight">
+    <div v-if="action === 'stand'" class="knight__block knight__block--stand">
+      <img src="../../assets/characters/knight/_Idle.png" class="knight__stand animate">
+    </div>
+    <div v-if="action === 'running'" class="knight__block knight__block--running">
+      <img src="../../assets/characters/knight/_Run.png" class="knight__running animate">
+    </div>
+    <div v-if="action === 'attacking'" class="knight__block knight__block--attacking">
+      <img src="../../assets/characters/knight/_Attack.png" class="knight__attacking animate">
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'knight',
+  props: {
+    action: {
+      type: String,
+      default: 'stand'
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+
+@keyframes animate {
+  from {
+    transform: translate3d(0px,0,0);
+  }
+  to {
+    transform: translate3d(-100%,0,0);
+  }
+}
+
+.knight{
+
+  $character-size: 500px;
+
+  // background-color: blue;
+
+  &__block {
+
+    height: 268px;
+
+    &--stand {
+      border: 2px solid greenyellow;
+      width: 131px;
+      overflow: hidden;
+
+      img {
+        position: relative;
+        top: -232px;
+        left: -275px;
+        animation: animate 1s steps(10) infinite;
+        height: $character-size;
+        image-rendering: pixelated;
+      }
+    }
+
+    &--running {
+      border:2px solid yellow;
+      width: 168px;
+      overflow: hidden;
+
+      img {
+        position: relative;
+        top: -232px;
+        left: -275px;
+        animation: animate .55s steps(10) infinite;
+        height: $character-size;
+        image-rendering: pixelated;
+      }
+    }
+
+    &--attacking {
+      border:2px solid purple;
+      width: 438px;
+      overflow: hidden;
+
+      img {
+        position: relative;
+        top: -232px;
+        left: -300px;
+        animation: animate .26s steps(4) infinite;
+        animation: animate .15s steps(4) infinite;
+        height: $character-size;
+        image-rendering: pixelated;
+      }
+    }
+  }
+}
+</style>
