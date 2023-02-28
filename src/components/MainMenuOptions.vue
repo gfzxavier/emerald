342bace77a0e7958ebff98f5a02ad2ab77b55371
Gabriel F. Xavier <gfzxavier@outlook.com>
2023-02-28 19:23:48 -0300

    feat: WIP add main menu components

---
+++ src/components/MainMenuOptions.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="main-menu-options">
+    <div class="main-menu-options__item">
+      Novo jogo
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainMenuTitle',
+}
+</script>
+
+<style lang="scss" scoped>
+
+@font-face {
+  font-family: Pixellari;
+  src: url('../assets/font/Pixellari.ttf');
+}
+
+.main-menu-options{
+  display: flex;
+  flex-direction: column;
+  font-family: Pixellari;
+  text-align: right;
+  color: rgb(255, 255, 255);
+  font-size: 70px;
+  padding-right: 15px;
+
+  &__item{
+    -webkit-text-stroke-width:4px;
+    -webkit-text-stroke-color: black;
+  }
+}
+</style>
---
+++ src/components/MainMenuTitle.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="main-menu-title">
+    <img src="../assets/img/logo.png" class="main-menu-title__logo">
+
+    <main-menu-options />
+  </div>
+</template>
+
+<script>
+import MainMenuOptions from './MainMenuOptions.vue'
+export default {
+  name: 'MainMenuTitle',
+  components: {
+    MainMenuOptions
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.main-menu-title{
+  position: absolute;
+  right: 60px;
+  top: 50px;
+
+  &__logo {
+    width: 580px;
+  }
+}
+</style>
