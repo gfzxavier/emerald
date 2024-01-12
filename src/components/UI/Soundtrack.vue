e4c35aff9232c8ceeb2323eb5091a72bb0afae74
Gabriel F. Xavier <gfzxavier@outlook.com>
2024-01-12 17:01:34 -0300

    feat: add soundtrack component

---
+++ src/components/UI/Soundtrack.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="soundtrack">
+    <audio autoplay>
+      <source :src="songUrl" type="audio/mpeg">
+    </audio>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'soundtrack',
+  props: {
+    song: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    songUrl(){
+      return `local-audio://sound/${this.song}.mp3`
+    }
+  },
+}
+</script>
+
+<style>
+
+</style>
